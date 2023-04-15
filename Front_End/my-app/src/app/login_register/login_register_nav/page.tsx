15676f28860bb2ad2684221181018af11f24247a
vngson <78072803+vngson@users.noreply.github.com>
2023-04-15 20:38:09 +0700

    "Login_Register part 2"

--- Front_End/my-app/src/app/login_register/login_register_nav/page.tsx
+++ Front_End/my-app/src/app/login_register/login_register_nav/page.tsx
@@ -1,23 +1,36 @@
+import { useState } from 'react';
 import classNames from 'classnames/bind';
 import Image from 'next/image'
 import styles from "./page.module.css"
 import logo from "@/assets/images/omrp_logo_transparent.png"
+import LoginForm from '@/app/login/page';
+import RegisterForm from '@/app/register/page';
 
 
 const cx = classNames.bind(styles);
 
 export default function Login_Register_Nav() {
+    const [ loginPage, setLoginPage] = useState(false)
+    const handleLogin = () => {
+        loginPage ? loginPage : setLoginPage(true);
+    }
+
+    const handleRegister = () => {
+        loginPage ? setLoginPage(false) : loginPage;
+    }
+
     return  (<div className={cx('login-register-nav')}>
         <div className={cx('nav-wrapper')}>
             <Image src={logo}  alt='logo' className={cx('nav__logo')}/>
             <div className={cx('nav__labels')}> 
-                <div className={cx("nav__label")} >
+                <div className={loginPage ? cx('nav__label-login') : cx('nav__label')} onClick={handleLogin}>
                     Đăng nhập
                 </div>
-                <div className={cx("nav__label")} >
+                <div className={loginPage ? cx('nav__label') : cx('nav__label-register')} onClick={handleRegister}>
                     Đăng ký
                 </div>
             </div>
+            <div className={cx('login-register__form')}>{ loginPage ? <LoginForm/> : <RegisterForm/>}</div> 
         </div>
     </div>) ;
 }
--- Front_End/my-app/src/app/login_register/page.tsx
+++ Front_End/my-app/src/app/login_register/page.tsx
@@ -5,13 +5,10 @@ import Image from 'next/image'
 import styles from "./page.module.css"
 import background from "@/assets/images/background.jpg"
 import Login_Register_Nav from './login_register_nav/page';
-import LoginForm from '../login/page';
-import RegisterForm from '../register/page';
 
 const cx = classNames.bind(styles);
 
 export default function LoginRegister() {
-    const [ loginPage, setLoginPage] = useState(false)
     return  (<div className={cx('login-register')}>
         <div className={cx('login-register-wrapper')}>
             <Image 
@@ -19,9 +16,7 @@ export default function LoginRegister() {
                 alt='background'
                 className={cx('login-register__background')}
             />
-            <div className={cx('login-register__nav')}><Login_Register_Nav/></div>
-            <div className={cx('login-register__form')}>{ loginPage ? <LoginForm/> : <RegisterForm/>}</div>
-            
+            <div className={cx('login-register__nav')}> <Login_Register_Nav /></div>
         </div>
     </div>) ;
 }
