82b15074d045b30e979f70026c537d017cee92d6
Tuyet Anh <84496906+tuyetanh0207@users.noreply.github.com>
2023-04-27 01:38:55 +0700

    reset master

--- Front_End/my-app/src/app/layout.tsx
+++ Front_End/my-app/src/app/layout.tsx
@@ -2,6 +2,8 @@
 import './globals.css'
 import { Provider } from "react-redux"
 import { store } from "@/redux/store"
+import Header from '@/components/header/page'
+import Footer from '@/components/footer/page'
 export const metadata = {
   title: 'OMRP - One milion reward point',
   description: 'One milion reward point',
@@ -16,7 +18,9 @@ export default function RootLayout({
     <html lang="en">
       <body>
       <Provider store={store}>
+        <Header/>
         {children}
+        <Footer/>
       </Provider>
       </body>
     </html>
--- Front_End/my-app/src/app/page.tsx
+++ Front_End/my-app/src/app/page.tsx
@@ -1,9 +1,9 @@
 import Image from 'next/image'
 import { Inter } from 'next/font/google'
 import styles from './page.module.css'
-import LoginRegister from '@/components/login_register/login_register'
-import Header from './header/page'
-import Footer from './footer/page'
+// import LoginRegister from '@/components/login_register/login_register'
+import Header from '../components/header/page'
+import Footer from '../components/footer/page'
 
 
 const inter = Inter({ subsets: ['latin'] })
@@ -12,7 +12,7 @@ export default function Home() {
   return (
     <main className={styles.main}>
       Home
-      <LoginRegister/>
+
     </main>
   )
 }
