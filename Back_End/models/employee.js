b9c9aa9e431c0ebcb1255261f1c40ac141117db2
hqthai <99537336+hqthai@users.noreply.github.com>
2023-05-31 21:39:02 +0700

    update API get contract

--- Back_End/models/employee.js
+++ Back_End/models/employee.js
@@ -43,7 +43,7 @@ exports.getListContractPendingApproval = async function (skip, limit) {
 exports.getContract = async function (id) {
   const client = await getClient();
   const rs = await client.query(
-    'SELECT CT."ID_CONTRACT",CT."DATE_CONTRACT" AS "Ngày lập HĐ" ,CT."EFFECTIVE_TIME" AS "Ngày hết HĐ",CT."AMOUNTTOPOINTS" AS "Đơn vị đổi",CT."COMMISSION" AS "% giao dịch",CT."CONTRACT_MANAGER" AS "Nhân viên quản lý hợp đồng",P."Name" AS "Tên doanh nghiệp",P."url" AS "Image"  FROM public."Contract" as CT , public."Partners" as P WHERE CT."CONTRACT_PARTNER" = P."ID_Partners" AND CT."ID_CONTRACT" = $1 ',
+    'SELECT CT."ID_CONTRACT",CT."TAX" as "Mã số thuế", CT."DATE_CONTRACT" AS "Ngày lập HĐ" ,CT."EFFECTIVE_TIME" AS "Ngày hết HĐ",CT."AMOUNTTOPOINTS" AS "Đơn vị đổi",CT."COMMISSION" AS "% giao dịch",P."Name" AS "Tên doanh nghiệp", P."Phone" as "Số điện thoại", P."Email",P."url" AS "Image" FROM public."Contract" as CT , public."Partners" as P WHERE CT."CONTRACT_PARTNER" = P."ID_Partners" AND CT."ID_CONTRACT" = $1 ',
     [id]
   );
   return rs.rows[0];
